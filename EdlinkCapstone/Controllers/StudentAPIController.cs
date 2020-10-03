34c29f2743619214dad60bb420aa134238fe522a
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-10-03 11:51:32 -0600

    Added Create Student API

---
+++ EdlinkCapstone/Controllers/StudentAPIController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EdlinkCapstone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EdlinkCapstone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        [HttpPost("AddStudent")]
+        public ActionResult<Student> AddStudent(string firstName, string lastName, string address, string email, string phoneNumber, DateTime dateOfBirth, int schoolID)
+        {
+            ActionResult<Student> response;
+            Student myStudent;
+           /* try
+            {*/
+                // We aren't concerned with validation here. Only in BLL.
+                myStudent = new StudentControllerBLL().CreateStudent(firstName, lastName, address, email, phoneNumber, dateOfBirth, schoolID);
+                // Encode our created object as JSON and bounce it back with the request.
+                response = Ok(myStudent);
+           /* }
+           /* catch (Exception e)
+            {
+                response = UnprocessableEntity(new { error = e.Message });
+            }
+           */
+            // Return the response.
+            return response;
+        }
+    }
+}
---
+++ EdlinkCapstone/Controllers/StudentControllerBLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EdlinkCapstone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EdlinkCapstone.Controllers
+{
+    public class StudentControllerBLL : Controller
+    {
+        public Student CreateStudent(string firstName, string lastName, string address, string email, string phoneNumber, DateTime dateOfBirth, int schoolID)
+        {
+            Student myStudent = new Student()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                Email = email,
+                PhoneNumber = phoneNumber,
+                DateOfBirth = dateOfBirth,
+                SchoolID = schoolID
+            };
+
+            using (SchoolContext context = new SchoolContext())
+            {
+                context.Students.Add(myStudent);
+                context.SaveChanges();
+            }
+           return myStudent;
+        }
+    }
+}
