097b20cb7d1f848b4a3aa4a6f2682a0273c1ed9c
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-10-06 09:50:42 -0600

    Deleted MDBReact

--- EdlinkCapstone/ClientApp/src/components/Footer.js
+++ EdlinkCapstone/ClientApp/src/components/Footer.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react' //react
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdbreact';
 import { Link } from 'react-router-dom';
 
-/*
-    Footer Component : Footer for all page
-
-*/
 const Footer = () => {
     return (
-        <MDBFooter color="blue" className="font-small pt-4 mt-4">
-            <div className="footer-copyright text-center py-3">
-                <MDBContainer fluid>
-                    &copy; {new Date().getFullYear()} Copyright: <Link to="/"> EDLINK </Link>
-                </MDBContainer>
-            </div>
-        </MDBFooter>
+        <div className="footer-copyright text-center py-3">
+            &copy; {new Date().getFullYear()} Copyright: <Link to="/"> EDLINK </Link>
+        </div>
     );
 }
 export default Footer;
