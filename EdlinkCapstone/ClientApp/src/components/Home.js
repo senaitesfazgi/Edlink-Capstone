7004cb02faa5b5466e0f0f2669c285baf713c660
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-10-07 21:55:00 -0600

    Removed signin and signup

--- EdlinkCapstone/ClientApp/src/components/Home.js
+++ EdlinkCapstone/ClientApp/src/components/Home.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Footer from './Footer';
-import { SignUp } from './sign-up/SignUp';
-import { SignIn } from './sign-in/SignIn';
 
 export class Home extends Component {
   static displayName = Home.name;
